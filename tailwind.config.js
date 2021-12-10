d3987d74b6399e1cf2b4f1fea2609418aac0ab72
shin thant <shinthant10684@gmail.com>
2021-12-10 23:34:58 +0630

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// * If you want to add something, then use your first name with it
+// * eg. s-red-10 / m-red-15
+
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
