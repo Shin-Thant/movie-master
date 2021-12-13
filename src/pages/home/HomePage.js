194e4a96e02c731d38f0cab4cd4c6965f731fec4
shin thant <shinthant10684@gmail.com>
2021-12-14 00:13:56 +0630

    add new roles

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -10,6 +10,8 @@ import "./homepage.css";
 import { GenreFilter } from "../../components/GenreFilter";
 import { useDispatch, useSelector } from "react-redux";
 import { changeStatus } from "../../redux/Actions/FilterActions";
+import { FiTrendingUp } from "react-icons/fi";
+import { FaFire } from "react-icons/fa";
 
 export const HomePage = () => {
     const { movieGenreIds, movies, movieStatus, movieLoading } = useSelector(
@@ -41,12 +43,73 @@ export const HomePage = () => {
         dispatch(changeStatus("series"));
     };
 
+    const [role, setRole] = useState("trend");
+
+    const trendsNow = () => {
+        setRole("trend");
+    };
+    const popular = () => {
+        setRole("popular");
+    };
+
     return (
         <div className="w-full mx-auto bg-black font-roboto">
             <HomePageIntro />
 
+            <div className="w-full mt-10">
+                <div className="w-11/12 sm:w-full mx-auto flex items-center mb-8 pb-6 types">
+                    <div
+                        onClick={trendsNow}
+                        className={
+                            role === "trend" ? " activeType" : "inactiveType"
+                        }
+                    >
+                        <FiTrendingUp
+                            className={
+                                role === "trend"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
+                            }
+                        />
+                        <span
+                            className={
+                                role === "trend"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
+                            }
+                        >
+                            Trends Now
+                        </span>
+                    </div>
+
+                    <div
+                        onClick={popular}
+                        className={
+                            role === "popular" ? "activeType" : "inactiveType"
+                        }
+                    >
+                        <FaFire
+                            className={
+                                role === "popular"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
+                            }
+                        />
+                        <span
+                            className={
+                                role === "popular"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
+                            }
+                        >
+                            Popular
+                        </span>
+                    </div>
+                </div>
+            </div>
+
             <div className="w-full sm:w-11/12 md:w-10/12 s_tablet:w-4/5 mx-auto mt-10 pb-5">
-                <div className="flex items-center mb-8 pb-6 types">
+                <div className="flex items-center mb-8 pb-6 types w-11/12 sm:w-full mx-auto">
                     <div
                         onClick={typeMovie}
                         className={
@@ -56,15 +119,15 @@ export const HomePage = () => {
                         <BiMoviePlay
                             className={
                                 active === "movie"
-                                    ? "text-lg sm:text-xl md:text-2xl font-bold"
-                                    : "text-base sm:text-base font-medium"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
                             }
                         />
                         <span
                             className={
                                 active === "movie"
-                                    ? "text-lg sm:text-xl md:text-2xl font-bold"
-                                    : "text-base sm:text-base font-medium"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
                             }
                         >
                             Movies
@@ -79,15 +142,15 @@ export const HomePage = () => {
                         <BsCollectionPlayFill
                             className={
                                 active === "tv"
-                                    ? "text-lg sm:text-xl md:text-2xl font-bold"
-                                    : "text-base sm:text-base font-medium"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
                             }
                         />
                         <span
                             className={
                                 active === "tv"
-                                    ? "text-lg sm:text-xl md:text-2xl font-bold"
-                                    : "text-base sm:text-base font-medium"
+                                    ? "text-lg sm:text-xl md:text-2xl font-bold transition-all duration-300"
+                                    : "text-base sm:text-base font-medium transition-all duration-300"
                             }
                         >
                             Series
